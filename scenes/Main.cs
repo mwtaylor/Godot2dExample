f319988baf097cb0ab013d8da1c6fe8fa439ec8e
Michael Taylor <mwtaylor@users.noreply.github.com>
2025-05-17 00:00:01 -0700

    Remove linear damp so slow mobs don't stop and remove all mobs on starting a new game

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -12,6 +12,8 @@ public partial class Main : Node
     {
         _score = 0;
         
+        GetTree().CallGroup("mobs", Node.MethodName.QueueFree);
+        
         var player = GetNode<Player>("Player");
         var startPosition = GetNode<Marker2D>("StartPosition");
         player.Start(startPosition.Position);
@@ -37,16 +39,17 @@ public partial class Main : Node
         var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
         mobSpawnLocation.ProgressRatio = GD.Randf();
 
-        float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+        var direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
         
         mob.Position = mobSpawnLocation.Position;
         
         direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
         mob.Rotation = direction;
 
-        var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+        var velocity = new Vector2((float)GD.RandRange(50.0, 250.0), 0);
         mob.LinearVelocity = velocity.Rotated(direction);
         
+        mob.AddToGroup("mobs");
         AddChild(mob);
     }
 
