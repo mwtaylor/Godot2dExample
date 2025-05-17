76e7d983f92891da0f0ac0e4842173920150c3e5
Michael Taylor <mwtaylor@users.noreply.github.com>
2025-05-16 22:05:24 -0700

    Add main scene

---
+++ scenes/Main.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+    [Export]
+    public PackedScene MobScene { get; set; }
+
+    private int _score;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        
+        _NewGame();
+    }
+
+    private void GameOver()
+    {
+        GetNode<Timer>("MobTimer").Stop();
+        GetNode<Timer>("ScoreTimer").Stop();
+    }
+
+    private void OnMobTimerTimeout()
+    {
+        var mob = MobScene.Instantiate<Mob>();
+
+        var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+        mobSpawnLocation.ProgressRatio = GD.Randf();
+
+        float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+        
+        mob.Position = mobSpawnLocation.Position;
+        
+        direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+        mob.Rotation = direction;
+
+        var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+        mob.LinearVelocity = velocity.Rotated(direction);
+        
+        AddChild(mob);
+    }
+
+    private void OnScoreTimerTimeout()
+    {
+        _score++;
+    }
+
+    private void OnStartTimerTimeout()
+    {
+        GetNode<Timer>("MobTimer").Start();
+        GetNode<Timer>("ScoreTimer").Start();
+    }
+
+    private void _NewGame()
+    {
+        _score = 0;
+        
+        var player = GetNode<Player>("Player");
+        var startPosition = GetNode<Marker2D>("StartPosition");
+        player.Start(startPosition.Position);
+        
+        GetNode<Timer>("StartTimer").Start();
+    }
+}
